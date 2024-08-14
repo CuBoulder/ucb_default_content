0966cf1d54503f221bb8a91b0e16940d7ec52b41
AlanBCole <alantuneit@gmail.com>
2024-08-14 13:17:11 +0200

    fix install order of service dependencies and code formatting

--- src/DefaultContent.php
+++ src/DefaultContent.php
@@ -3,15 +3,18 @@
 namespace Drupal\ucb_default_content;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\node\Entity\Node;
 use Drupal\pathauto\PathautoState;
 
+/**
+ * Class for creating default content when creating sites.
+ */
 class DefaultContent {
 
   /**
-   * The config factory
+   * The config factory.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
@@ -34,25 +37,27 @@ class DefaultContent {
   /**
    * Constructs DefaultContent service.
    *
-   * @param ConfigFactoryInterface $config_factory
-   * @param Connection $database
+   * @param Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   Used to save configuration for created default content.
+   * @param Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   Used for logging.
+   * @param Drupal\Core\Database\Connection $database
+   *   Database connection used for setting sitemap data.
    */
   public function __construct(
-    ConfigFactoryInterface $config_factory,
-    Connection $database,
+    ConfigFactoryInterface $configFactory,
     LoggerChannelFactoryInterface $logger,
+    Connection $database,
   ) {
-    $this->configFactory = $config_factory;
-    $this->database = $database;
+    $this->configFactory = $configFactory;
     $this->logger = $logger;
+    $this->database = $database;
   }
 
   /**
    * Create the default homepage.
-   *
-   * @return void
    */
-  public function create_homepage() {
+  public function createHomepage() {
     $node = Node::create([
       'type' => 'basic_page',
       'title' => 'Home',
@@ -63,15 +68,13 @@ class DefaultContent {
     $node->enforceIsNew()->save();
     $nid = $node->id();
     $this->configFactory->getEditable('system.site')->set('page.front', '/node/' . $nid)->save();
-    $this->set_simplesitemap_no_index($nid);
+    $this->setSimplesitemapNoIndex($nid);
   }
 
   /**
    * Create the default 404 page.
-   *
-   * @return void
    */
-  public function create_404_page() {
+  public function create404Page() {
     $node = Node::create([
       'type' => 'basic_page',
       'title' => 'Page Not Found',
@@ -81,22 +84,21 @@ class DefaultContent {
     $node->enforceIsNew()->save();
     $nid = $node->id();
     $this->configFactory->getEditable('system.site')->set('page.404', '/node/' . $nid)->save();
-    $this->set_simplesitemap_no_index($nid);
+    $this->setSimplesitemapNoIndex($nid);
   }
 
   /**
-   * Undocumented function
+   * Make sure sitemap knows about new default content.
    *
    * @param string $nid
    *   The id of the node.
-   * @return void
    */
-  private function set_simplesitemap_no_index($nid) {
+  private function setSimplesitemapNoIndex($nid) {
     // This method may not be part of the public API but there is no other clean
     // way of doing this. Simple XML Sitemap still uses a custom
-    // `simple_sitemap_entity_overrides` DB table to set these overrides. There's
-    // an issue to store these overrides as fields on the entity but it's been
-    // open since 2019 (!!!):
+    // `simple_sitemap_entity_overrides` DB table to set these overrides.
+    // There's an issue to store these overrides as fields on the entity
+    // but it's been open since 2019 (!!!):
     // https://www.drupal.org/project/simple_sitemap/issues/3034070
     // BEWARE OF BREAKING CHANGES to Simple XML Sitemap.
     if ($this->database->schema->tableExists('simple_sitemap_entity_overrides')) {
@@ -123,4 +125,5 @@ class DefaultContent {
       $this->logger->get('ucb_default_content')->warning('Failed to set Simple XML Sitemap no index (table doesn\'t exist). Simple XML Sitemap isn\'t installed or changed something in an update.');
     }
   }
+
 }
